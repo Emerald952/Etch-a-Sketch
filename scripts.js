d161662d5058fce9083f5fe0f26fa92abdff48fa
Ashkid <sushmitapant9@gmail.com>
2025-07-25 23:22:22 +0530

    Added Shading pen and Rainbow Pen

--- scripts.js
+++ scripts.js
@@ -1,65 +1,115 @@
 let currGridSize = 16;
 let isDrawing = false;
-let pixel_color = 'black';
+let pixelColor = 'black';
+let isRainbow = false;
+let isShading = false;
 
-const sketchpad = document.querySelector('#sketchpad');
-const gridSelect = document.querySelector('#select-grid');
-const color = document.querySelector('#color');
+const sketchPad = document.querySelector('#sketchPad');
+const gridSelect = document.querySelector('#selectGrid');
+const penColor = document.querySelector('#penColor');
+const gridColor = document.querySelector('#gridColor');
 const eraser = document.querySelector("#eraser");
+const rainbow = document.querySelector("#rainbowPen");
+const shading = document.querySelector("#shadingPen");
 
-color.addEventListener('input', (e) => {
-    pixel_color = e.target.value;
+penColor.addEventListener('input', (e) => {
+    pixelColor = e.target.value;
+    isRainbow = false;
+    isShading = false;
+});
+
+gridColor.addEventListener('input', (e) =>{
+    sketchPad.style.backgroundColor = e.target.value;
 });
 
 eraser.addEventListener("click", () => {
-    pixel_color = 'white';
+    pixelColor = 'white';
+    isRainbow = false;
+    isShading = false;
 });
 
-sketchpad.addEventListener("mousedown", ()=>{
+rainbow.addEventListener("click", () => {
+    isRainbow = true;
+    isShading = false;
+});
+
+shading.addEventListener("click", () => {
+    sketchPad.style.backgroundColor = 'white';
+    isShading = true;
+    isRainbow = false;   
+});
+
+sketchPad.addEventListener("mousedown", ()=>{
     isDrawing = true;
 });
-sketchpad.addEventListener("mouseup", ()=>{
+sketchPad.addEventListener("mouseup", ()=>{
     isDrawing = false;
 });
-sketchpad.addEventListener("mouseleave", ()=>{
+sketchPad.addEventListener("mouseleave", ()=>{
     isDrawing = false;
 });
 
 gridSelect.addEventListener('change', (e)=>{
     const newGridSize = parseInt(e.target.value);
     if(newGridSize != currGridSize){
-        pixel_color = color.value;
+        pixelColor = penColor.value;
         currGridSize = newGridSize;
         createGrids(currGridSize);
     }
 });
         
 function createGrids(gridSize){
-    sketchpad.innerHTML = '';
+    sketchPad.innerHTML = '';
     const totalPixel = gridSize*gridSize;
-    const pixelsize = 500 / gridSize;
+    const pixelSize = 500 / gridSize;
     for(let i = 0; i < totalPixel; i++){
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
-        pixel.style.width = `${pixelsize}px`
-        pixel.style.height = `${pixelsize}px`
+        pixel.style.width = `${pixelSize}px`
+        pixel.style.height = `${pixelSize}px`
         
+        pixel.alpha = 0;
+
         pixel.addEventListener('mousedown', ()=>{
-            pixel.style.backgroundColor = pixel_color;
+            if(isRainbow){
+                const red = Math.floor(Math.random()*256); 
+                const green = Math.floor(Math.random()*256);
+                const blue = Math.floor(Math.random()*256);
+                pixelColor = `rgb(${red}, ${green}, ${blue})`;
+            }
+            else if(isShading){
+                pixel.alpha = Math.min(pixel.alpha + 0.1, 1);
+                pixelColor = `rgba(0, 0, 0, ${pixel.alpha})`;
+            }
+            pixel.style.backgroundColor = pixelColor;
+
         });
 
         pixel.addEventListener('mouseover', ()=>{
             if(isDrawing){
-                pixel.style.backgroundColor = pixel_color;
+                if(isRainbow){
+                    const red = Math.floor(Math.random()*256); 
+                    const green = Math.floor(Math.random()*256);
+                    const blue = Math.floor(Math.random()*256);
+                    pixelColor = `rgb(${red}, ${green}, ${blue})`;
+                }
+                else if(isShading){
+                    sketchPad.style.backgroundColor='white';pixel.alpha = Math.min(pixel.alpha + 0.1, 1);
+                    pixelColor = `rgba(0, 0, 0, ${pixel.alpha})`;
+                }
+                pixel.style.backgroundColor = pixelColor;
             }
         });
 
-        sketchpad.appendChild(pixel);
+        sketchPad.appendChild(pixel);
 
     }
 }
 
-document.querySelector('#clear-all').addEventListener('click', () => {
+document.querySelector('#clearAll').addEventListener('click', () => {
     createGrids(currGridSize);
+    isRainbow = false;
+    isShading = false;
+    pixelColor = 'black';
 });
 createGrids(16);
